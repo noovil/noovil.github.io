6fdace78fba89fb990cf4055f64c6024506874d9
noovil <lokelivoon@gmail.com>
2022-03-12 19:07:39 +0800

    update portfolio

---
+++ contact_form.js
@@ -0,0 +1,7 @@
+
+function sendMessage() {
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const message= document.getElementById("message").value
+
+}
